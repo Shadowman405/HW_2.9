8f68356b7e9d2effd6248a863fbf3bf63d091b2b
Maxim Mitin <maximus405@gmail.com>
2021-08-03 22:51:00 +0300

    init

--- HW_2.9/ViewController.swift
+++ HW_2.9/ViewController.swift
@@ -5,15 +5,83 @@
 //  Created by Maxim Mitin on 3.08.21.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewToAnimate: SpringView!
+    @IBOutlet weak var animationNameLbl: UILabel!
+    @IBOutlet weak var animationButtonLbl: SpringButton!
+    
+    
+    var currentAnimationIndex = -1
+    var currentBtnName = 0
+    
+    let enu = Spring.AnimationPreset.self
+    let animations: [Spring.AnimationPreset] = [
+        .shake,
+        .pop,
+        .morph,
+        .squeeze,
+        .wobble,
+        .swing,
+        .flipX,
+        .flipY,
+        .fall,
+        .squeezeLeft,
+        .squeezeRight,
+        .squeezeDown,
+        .squeezeUp,
+        .slideLeft,
+        .slideRight,
+        .slideDown,
+        .slideUp,
+        .fadeIn,
+        .fadeOut,
+        .fadeInLeft,
+        .fadeInRight,
+        .fadeInDown,
+        .fadeInUp,
+        .zoomIn,
+        .zoomOut,
+        .flash
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startValues()
     }
-
-
+    
+    private func nextAnim() {
+        currentAnimationIndex += 1
+        currentBtnName += 1
+        if currentAnimationIndex >= animations.count || currentBtnName >= animations.count {
+            currentAnimationIndex = 0
+            currentBtnName = 1
+        } else {
+        viewToAnimate.animation = "\(animations[currentAnimationIndex])"
+        animationNameLbl.text = "\(animations[currentAnimationIndex])"
+        viewToAnimate.force = 2
+        viewToAnimate.duration = 4
+        viewToAnimate.delay = 0.2
+        viewToAnimate.animate()
+        print(animations.count)
+        print(currentAnimationIndex)
+            print(currentBtnName)
+        
+        animationButtonLbl.setTitle("\(animations[currentBtnName])", for: .normal)
+        }
+    }
+    
+    private func startValues() {
+        guard let lblName = animations.first else {return}
+        guard let btnName = animations.first else {return}
+        
+        animationNameLbl.text = "\(lblName)"
+        animationButtonLbl.setTitle("\(btnName)", for: .normal)
+    }
+    
+    @IBAction func animateBtnClicked(_ sender: SpringButton) {
+        nextAnim()
+ }
 }
-
